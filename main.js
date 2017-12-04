65bf30b82a8d00486cdb56edb35b86d798ebb0fd
kalinbowden <kalinbowden@gmail.com>
2017-12-04 14:38:18 -0600

    added a new menu and a working submit item form. Still need  to finish video

--- main.js
+++ main.js
@@ -6,10 +6,17 @@
 
 // Constants for the instance of the Electron window
 const electron = require('electron');
+const url = require('url');
+const path = require('path');
+
 const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
 const Menu = electron.Menu;
 const ipc = electron.ipcMain;
+
+//
+let mainMenu;
+let addWindow;
 
 //  Create the main GUI window for the app
 app.on('ready', _=>{
@@ -17,7 +24,7 @@ app.on('ready', _=>{
     console.log('TaskList has started');
 
     // Create the window
-    mainWindow = new BrowserWindow({ width:300, height:200});
+    mainWindow = new BrowserWindow({ width:1000, height:650});
 
     //
     const menu = Menu.buildFromTemplate(template);
@@ -30,13 +37,51 @@ app.on('ready', _=>{
     mainWindow.on('closed', _=>{mainWindow = null; console.log('TaskList has closed');})
 })
 
+//
+//mainWindow.on('closed', _=>{app.quit();});
+
+// Garbage Collection
+//addWindow.on('closed', _=>{addWindow = null;});
+
+
+// Instanciate a new window
+function createAddWindow()
+{
+    // Starting-up notification
+    console.log('AddWindow has started');
+
+    // Create the window
+    addWindow = new BrowserWindow({ width:450, height:300});
+
+    //
+    const menu = Menu.buildFromTemplate(template);
+    Menu.setApplicationMenu(menu);
+
+    //
+    addWindow.loadURL(`file://${__dirname}/addWindow.html`);
+
+    // Destroy window
+    addWindow.on('closed', _=>{addWindow = null; console.log('AddWindow has closed');})
+}
+
+// catch item add
+ipc.on('item:add', function(e, item)
+{
+    console.log(item);
+    mainWindow.webContents.send('item:add', item)
+    addWindow.close();
+    
+})
+
+
 const template =
 [
     {label: electron.app.getName(), submenu: 
         [
-            {label: 'About'},
+            {label: 'Add Item', click: _=>{createAddWindow()}},
+            {label: 'Clear Items'},
             {type: 'separator'}, 
-            {label: 'Quit', click: _=>{app.quit()},accelerator: 'Crlt+Q'}
+            {label: 'Quit', click: _=>{app.quit()},accelerator: 'Ctrl+Q'}
         ]},
         {
             label: 'Dev Tools',
@@ -59,3 +104,19 @@ ipc.on('countdown-start', (evt, arg) =>
         count--;
     }, 1000);
 })
+
+
+// Add developer tools when developing
+if (process.env.NODE_ENV !== 'production')
+{
+    template.push({
+        label: 'Developer Tools',
+        submenu:
+        [
+            {label: 'Toggle DevTools', click(item, focusedWindow)
+            {
+                focusedWindow.toggleDevTools();
+            }}
+        ]
+    })
+}
